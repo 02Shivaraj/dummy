37a755c78b09468b9900706fddd0b2018ef4f868
Ankush Narayan Balse <123646775+Ankush-Balse@users.noreply.github.com>
2023-11-20 23:05:21 +0530

    Update Myindex.js

--- js/Myindex.js
+++ js/Myindex.js
@@ -47,7 +47,7 @@ document.getElementById("button").addEventListener("click",function(){
     if(x === "START")
         game();
     else{
-        gameOver = true;
+        gameStopped = true;
         alert("Game Stopped!")
     }
 });
@@ -75,6 +75,9 @@ function forMobile(){
 
         board.style.width="90vw";
         board.style.height="90vw";
+
+        var x=screen.width;
+        console.log(x);
     }
     else{
         controls.classList.remove("controls");
@@ -86,9 +89,19 @@ function forMobile(){
 }
 
 let gameOver = false;
+let gameStopped = false;
 var h_sc=0;
 
+const foodSound = new Audio("music/food.mp3");
+const overSound = new Audio("music/gameover.mp3");
+const moveSound = new Audio("music/move.mp3");
+
 function game(){
+    if(overAlert.style.display==="block")
+        return;
+
+    const gameSound = new Audio("music/music.mp3");
+    gameSound.play();
     change();
     let inputDir = [1,0]; 
     let snakeArr = [[4,2],[3,2],[2,2]];
@@ -99,9 +112,9 @@ function game(){
     let b=size-1;
     let sc=0;
 
-    let currCycle=0;
     let prevDirChange=0;
-    
+    let currCycle=0;
+
     food = [Math.round(a + (b-a)* Math.random()),Math.round(a + (b-a)* Math.random())];
     
     //check if mobile device
@@ -109,13 +122,20 @@ function game(){
     
     function main(ctime) {
         //Reset game if Game Over
-        if(gameOver){
-            gameOver = false;
+        if(gameOver || gameStopped){
             inputDir =  [1,0]; 
             snakeArr = [[4,2],[3,2],[2,2]]; 
             change();  
             initial();
             score.innerHTML=0;
+            if(gameOver){
+                gameOver=false;
+                i=0;
+                while(i<100000)
+                    i=i+0.003;
+                overAlert.style.display="block";
+            }
+            gameStopped = false;
             return;
         }
         window.requestAnimationFrame(main);
@@ -137,8 +157,10 @@ function game(){
     function gameEngine(){
         ++currCycle;
         if(isCollide(snakeArr)){
-            gameOver = true;   
-            alert("Game Over. Press any key to play again!");
+            overSound.play();
+            gameSound.pause();
+            board.classList.add("shake");      
+            gameOver = true;
         }
         else{
             // Part 1: Updating the snake array & Food
@@ -168,7 +190,7 @@ function game(){
             }     
             // If you have eaten the food, increment the score and regenerate the food
             if(snakeArr[0][1] === food[1] && snakeArr[0][0] ===food[0]){
-                //snakeArr.unshift([snakeArr[0][0] + inputDir[0], snakeArr[0][1] + inputDir[1]]);
+                foodSound.play();
                 snakeArr[snakeArr.length] = [0,0];
                 food = [Math.round(a + (b-a)* Math.random()),Math.round(a + (b-a)* Math.random())]
                 while(snakeArr.includes(food)){                
@@ -263,12 +285,15 @@ function game(){
             board.appendChild(foodElement);
         }   
     }
+
     window.requestAnimationFrame(main); 
 
     function moveSnake(e){
+        console.log("moveSnake");
         if(prevDirChange === currCycle)
             return;
         prevDirChange = currCycle;
+        moveSound.play();
         if(e=="ArrowUp" && inputDir[1]!=1){
             inputDir[0]=0;
             inputDir[1]=-1;
@@ -300,4 +325,53 @@ function game(){
         keyBtn.addEventListener('mousedown', handleButtonKeyMove);
         keyBtn.addEventListener('touchstart', handleButtonKeyMove);
     });
+
+    document.addEventListener('touchstart', handleTouchStart, false);        
+    document.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;                                                        
+    var yDown = null;
+
+    function getTouches(evt) {
+        return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+    }                                                     
+
+    function handleTouchStart(evt) {
+        const firstTouch = getTouches(evt)[0];                                      
+        xDown = firstTouch.clientX;                                      
+        yDown = firstTouch.clientY;                                      
+    };                                                
+
+    function handleTouchMove(evt) {
+        if ( ! xDown || ! yDown ) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;                                    
+        var yUp = evt.touches[0].clientY;
+
+        var xDiff = xDown - xUp;
+        var yDiff = yDown - yUp;
+
+        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+            if (xDiff <= 0 && inputDir[0]!=1 ) {
+                inputDir[0]=1;
+                inputDir[1]=0; 
+            } else if(xDiff > 0 && inputDir[0]!=-1){
+                inputDir[0]=-1;
+                inputDir[1]=0;
+            }                       
+        } else {
+            if (yDiff <= 0 && inputDir[1]!=1) {
+                inputDir[0]=0;
+                inputDir[1]=1; 
+            } else if(yDiff > 0 && inputDir[1]!=-1){ 
+                inputDir[0]=0;
+                inputDir[1]=-1;
+            }                                                                 
+        }
+        xDown = null;
+        yDown = null;                                             
+    };
 }
